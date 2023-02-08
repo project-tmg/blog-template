c0fad0b71198505528fd7dc694e92a48563c1a2f
project-tmg <rudra.project.tmg@gmail.com>
2023-02-08 22:47:05 +0000

    build 0.1 in-progress

--- app.js
+++ app.js
@@ -15,19 +15,28 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+//process
+app.get("/", function(req, res) {
+    res.render("home", {startingContent: homeStartingContent});
+});
 
+app.get("/about", function(req, res) {
+    res.render("about", {startingContent: aboutContent});
+});
 
+app.get("/contact", function(req, res) {
+    res.render("contact", {startingContent: contactContent});
+});
 
+app.get("/compose", function(req, res) {
+    res.render("compose");
+});
+app.post("/compose", function(req, res) {
+    console.log(req.body);
+    res.render("home", {startingContent: homeStartingContent});
+});
 
-
-
-
-
-
-
-
-
-
+//listet
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
